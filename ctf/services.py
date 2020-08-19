5324db31b457375ef1d082cbb6dce67fa6368bc6
Zkn <dmzkn@ya.ru>
2020-08-19 12:51:57 +0300

    available_flags logic

--- ctf/services.py
+++ ctf/services.py
@@ -12,19 +12,28 @@ def is_captured(flag, user):
 
 
 def available_flags(user, order_by='score'):
-    return Flag.objects \
-        .filter( 
-            Q(chain__parent__in= \
-                Flag.objects \
-                    .filter(
-                        submit__captured=True
-                    ) \
-                    .filter(
-                        submit__user=user
-                    )
-            ) |
-            Q(chain__parent__exact=None)
-        ).order_by(order_by)
+    if user.is_superuser:
+        # admin see everything
+        flags = Flag.objects.all()
+    else:
+        parent_captured = Q(chain__parent__in= \
+            Flag.objects.filter(
+                Q(submit__captured=True) & Q(submit__user=user)
+                ))
+        no_parent = Q(chain__isnull=True)
+        parent_disabled = Q(chain__parent__enabled=False)
+        chain_enabled = Q(chain__enabled=True)
+        flags = Flag.objects \
+            .filter(
+                Q(((parent_captured | parent_disabled) & chain_enabled) | no_parent)
+            ) \
+            .filter(
+                enabled=True
+            )
+    # exclude disabled groups
+    flags.exclude(group__enabled=False)
+    flags.order_by(order_by)
+    return flags
 
 
 def save_submit(user, flag, submit_value):
